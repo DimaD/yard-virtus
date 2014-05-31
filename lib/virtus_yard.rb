d2d53912e4eda2617ce269a6233546de25423bca
Dmitry Dzema <dmitriy@dzema.name>
2014-06-01 00:06:49 +0700

    Add VERSION constant

--- lib/virtus_yard.rb
+++ lib/virtus_yard.rb
@@ -1,6 +1,8 @@
 require "virtus"
 require "yard"
 
+require File.join(File.dirname(__FILE__), "virtus_yard", "version")
+
 require File.join(File.dirname(__FILE__), "virtus_yard", "handlers")
 require File.join(File.dirname(__FILE__), "virtus_yard", "declarations")
 require File.join(File.dirname(__FILE__), "virtus_yard", "code_objects")
---
+++ lib/virtus_yard/version.rb
@@ -0,0 +1,3 @@
+module VirtusYARD
+  VERSION = "0.0.1"
+end
