59c25f26f3ddf83b6b02b8b11f911ffa6381686f
Dmitry Dzema <dmitriy@dzema.name>
2014-05-31 22:57:47 +0700

    Improve documentation for `have_return_type` rspec matcher

--- spec/support/matchers/method_type_matchers.rb
+++ spec/support/matchers/method_type_matchers.rb
@@ -1,9 +1,11 @@
-RSpec::Matchers.define :have_return_type do |type|
+# @example
+#   expect(YARD::Registry.at("User#friends")).to have_return_type("Array<User>")
+RSpec::Matchers.define :have_return_type do |yard_type_string|
   match do |method_object|
     return_tags = method_object.tags(:return)
 
     expect(return_tags.size).to be > 0
 
-    expect(return_tags.any? { |tag| tag.types.include?(type) }).to be_true
+    expect(return_tags.any? { |tag| tag.types.include?(yard_type_string) }).to be_true
   end
 end
