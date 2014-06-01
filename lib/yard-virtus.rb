c86be1e8dc044a2a3be779b24c536aeb7647f444
Dmitry Dzema <dmitriy@dzema.name>
2014-06-01 16:35:39 +0700

    Add a little rant about library name and how it should be required

--- lib/yard-virtus.rb
+++ lib/yard-virtus.rb
@@ -7,3 +7,12 @@ require File.join(_dir, "yard", "virtus", "version")
 require File.join(_dir, "yard", "virtus", "handlers")
 require File.join(_dir, "yard", "virtus", "declarations")
 require File.join(_dir, "yard",  "virtus", "code_objects")
+
+# According to ruby conventions if library is called
+# `yard-virtus` it should be included via
+#
+#     require "yard/virtus"
+#
+# But YARD plugins do not follow this convention and require
+# gem names like `yard-*` and being required by the same name.
+# Hence this confusing filename.
